bce852d29c2aabb7b6ff7aac0e4ff3a91d598a83
Apparao <apparao.yala@syenainfosoft.com>
2026-09-08 17:53:16 +0530

    mail

--- Utilities/MailUtil.ts
+++ Utilities/MailUtil.ts
@@ -541,7 +541,7 @@ if (process.env.EMAIL_CREDENTIALS) {
         // });
       await transporter.sendMail({
     from: emailUser,
-    to: process.env.EMAIL_TO || 'vimala.salana@hospiquesoftware.com'||'Kranthi.Arumulla@hospiquesoftware.com'||'Apparao.Yala@hospiquesoftware.com',
+    to: process.env.EMAIL_TO,
 
     subject:
         failedTests > 0
